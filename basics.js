eff471ebab792bb4b9a57955f2c3846ac454b27c
GayatriM66 <gayatrimahale1990@gmail.com>
2022-10-07 15:46:26 +0530

    basics js

---
+++ basics.js
@@ -0,0 +1,67 @@
+// let a = 10
+// let b = a
+// b=45  //value of b is updated
+
+// console.log(a,b)
+
+
+
+// //array
+// let arr1=[10,20,30]
+// let arr2=arr1
+// arr2[0]=677     //updated for both arrays as they are in the heap memory,
+//  //and address is shared for both arrays
+// console.log(arr1,arr2)
+
+
+
+
+
+
+
+// var obj = {}
+
+// obj.message = ["hello"]
+// //obj["message"]=["hello"]
+// console.log(obj)
+
+
+
+
+
+// function string(greet, message) {
+//     console.log(greet + message)
+// }
+
+// string("good morning!!!", "gayatri");
+
+
+
+
+// let person1={
+//     name:"abc",
+//     rn:30,
+//     age:20
+// }
+
+// function printobj(obj){
+//     for(let key in obj){
+//         console.log(key,obj[key])
+//     }
+// }
+// printobj(person1);
+
+
+
+
+
+
+
+// let person2={
+//     age:33,
+//     rn:77
+// }
+
+// for(let key in person2){
+//     console.log(key, person2[key])
+// }
