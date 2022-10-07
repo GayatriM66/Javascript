7821938cd87540b3eba93c2810f028ef539e4e42
GayatriM66 <gayatrimahale1990@gmail.com>
2022-10-07 13:39:03 +0530

    arithmetic comparison

---
+++ arithmetic.js
@@ -0,0 +1,20 @@
+//----logical operation
+//----+,-,*,/,%
+
+//-----comparison operation
+//-----<,>,<=,>=,==,===,!=,!==          (= and ! alone are not comaprison operators)
+
+let x=10, y=50
+
+let z =x/y
+console.log("\n 10/50 is ",z)
+
+z =x%7
+console.log("\n 10%50 is ",z ,"\n")  //% prints remainder
+
+console.log("7==7 is ",7==7)              //true
+console.log("7===7 is ", 7===7)           //false
+console.log("7!=7 is ", 7!=7)             //false
+console.log("'7'==7 is ",'7'==7)          //true
+console.log("7 !=='7' is ", 7 !=='7')      //true
+console.log("7 !==7 is ", 7 !==7)         //true
