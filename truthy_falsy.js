1a144d74a5959f56e8ec9c6c86e8b2dbd5d44081
GayatriM66 <gayatrimahale1990@gmail.com>
2022-10-07 22:42:49 +0530

    truthy and falsy values

---
+++ truthy_falsy.js
@@ -0,0 +1,37 @@
+//truthy and falsy values
+
+//In JavaScript, a truthy value is a value that is considered true when encountered in a Boolean context.
+// All values are truthy unless they are defined as falsy. That is, 
+//all values are truthy except:
+//false, 0, -0, 0n, "", null, undefined, and NaN.
+
+
+
+
+console.log(Boolean('add'))//true
+console.log(Boolean(false))//false
+console.log(Boolean(0))//false
+console.log(Boolean(-0))//false
+console.log(Boolean(0n))//false
+console.log(Boolean(null))//false
+console.log(Boolean(NaN))//false
+console.log(Boolean(undefined))//false
+console.log(Boolean("")) //false
+console.log(Boolean(" ")) //true
+console.log(Boolean('')) //false
+console.log(Boolean(' ')) //true
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
