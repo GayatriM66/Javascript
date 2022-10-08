64dcdf92f48952ed132ec18e45e8fed3882d0ed4
GayatriM66 <gayatrimahale1990@gmail.com>
2022-10-08 13:34:15 +0530

    prime number

---
+++ logical/primeNumber.js
@@ -0,0 +1,22 @@
+// let count,i;
+// function primeNo(num) {
+//     if(num<2){
+//         return "the number is not prime"
+//     }
+//     for (i = 2; i < num; i++) {
+//         if (num % i === 0) {
+//             return "the number is not prime"
+//         }
+//     }
+//     return "is a prime"
+// }
+// console.log(primeNo(13))
+
+// //learn break
+// for(let i=1;i<=5;i++){
+//     console.log("this is for i ",i)
+//     for(let j=i;j<=5;j++){
+//         console.log("executing break", j)
+//         break //breaks the current loop only
+//     }
+// }
