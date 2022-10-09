a8b65429f60a82f91072ca2e77fa65a09ed7f2e5
GayatriM66 <gayatrimahale1990@gmail.com>
2022-10-09 07:30:20 +0530

    types of function

---
+++ typesOfFunction.js
@@ -0,0 +1,186 @@
+// let x = 11
+// console.log("value of x is ", x);
+
+// let y = 20
+// y = 22
+// //=--------------arithmetic operation-------------------
+// console.log("value of y is ", y)
+
+// console.log(x+y)
+// console.log(x-y)
+// console.log(x*y)
+// console.log(x/y)
+// console.log(x%y)
+
+
+
+
+
+// //---------------------k j ------------------
+// let k=100, j=200
+
+// console.log("valueof k is ", k)
+// console.log("value of j is ",j)
+// console.log(k+j)
+// console.log(k-j)
+// console.log(k*j)
+// console.log(k/j)
+// console.log(k%j)
+
+
+
+
+
+
+// //--------------function without parameter without return type----------------
+//  k=100,j=200;
+// function add(){
+//     console.log("addition of k and j is ",k+j)
+//                  taking value from line 19 where k , j declared
+// }
+// add()
+
+
+
+
+
+
+//// //----------function with parameter without returntype----------
+// //-------------------function cal() k j----------------------
+// function cal(k,j){
+//     console.log(k+j)
+//     console.log(k-j)
+//     console.log(k*j)
+//     console.log(k/j)
+//     console.log(k%j)
+// }
+
+// cal(10,20)
+// cal (100,140)
+
+
+
+
+
+
+
+
+
+
+// //=-------function without parameter with returntype--------
+
+// function mul()
+// {
+//    return(k*j)
+// }
+
+// let a=mul();
+// console.log("multiplication of k * j is ", a )
+
+
+
+
+
+
+
+
+// //-------function with parameter and with returntype----------------
+
+// function div(k,j)
+// {
+//     return (k/j)
+// }
+// let b = div(100,200)
+// console.log("division of k / j is ", b)
+
+
+
+
+
+
+// //function() declaration
+// let m=30, n=40
+
+// function addition(m,n){
+//     return m+n
+// }
+// let vv=addition(10,20)
+// console.log(vv)     //30, 10=20 arguments
+
+
+
+
+
+
+
+// //function() expression
+//it is a anonymous function, which is assigned to a variable addition1
+////imp * we can treat addition1(20,12) as a function here
+
+// let addition1 =function(m,n) {       
+//     return m+n
+// }
+// let result=addition1(20,12)   
+// console.log("function expression",result) //////20-12 is 8
+
+
+
+
+
+
+
+// //------------example of function expression
+// let add5 = function (n1, n2) {
+//     return n1 + n2
+// }
+// let sub5 = function (n1, n2) {
+//     return n1 - n2
+// }
+
+// let result1 = add5(5, 7) + sub5(5, 4)     //12+1= 13
+
+// console.log("result is ", result1);
+
+
+
+
+
+
+
+// //arrow function(), no function keyword is written in arrow function
+// //arrow function is anonymous
+// //without return
+
+//no need to write return as there is only one return statement
+//if function have a one statement no need to put {}
+// let yy = (m, n) => 
+//     m + n
+
+// let zz = yy(5, 10)
+// console.log(zz)   //15
+
+
+
+//||or||
+
+
+
+
+//with return
+// let yy = (m, n) =>{
+//     return m + n
+// }
+
+// let zz = yy(5, 10)
+// console.log(zz)   //15
+
+
+
+
+
+
+
+// //another example for arrow function
+// let add6 = (p1, p2) => p1 + p2    
+
+// console.log(add6(5, 6))                  
